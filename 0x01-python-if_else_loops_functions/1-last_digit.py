f4d172b3e22edf7e38a3a7b168d9ef93d181fa7b
DevHeart1 <obasipeace412@yahoo.com>
2023-10-04 16:40:08 -0700

    added files

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+# We first convert the number to a string
+number_string = str(number)
+
+# Then extract the last digit
+last_digit = number_string[-1]
+
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        k = ord(c)
