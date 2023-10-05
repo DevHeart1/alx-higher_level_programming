ca62286af787e87069ddb54f9cd2ab5ce53d48cf
DevHeart1 <obasipeace412@yahoo.com>
2023-10-05 16:48:47 -0700

    added task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    c = add(a, b)
 
-a = 10
-b = 5
-c = add(a, b)
- 
-print('{:d} + {:d} = {:d}'.format(a, b, c))
+    print('{:d} + {:d} = {:d}'.format(a, b, c))
 
+    a = 10
+    b = 5
+    c = sub(a, b)
 
-a = 10
-b = 5
-c = sub(a, b)
+    print('{:d} - {:d} = {:d}'.format(a, b, c))
 
-print('{:d} - {:d} = {:d}'.format(a, b, c))
+    a = 10
+    b = 5
+    c = mul(a, b)
 
+    print('{:d} * {:d} = {:d}'.format(a, b, c))
 
-a = 10
-b = 5
-c = mul(a, b)
+    a = 10
+    b = 5
+    c = div(a, b)
 
-print('{:d} * {:d} = {:d}'.format(a, b, c))
+    print('{:d} / {:d} = {:d}'.format(a, b, c))
 
-
-a = 10
-b = 5
-c = div(a, b)
-
-print('{:d} / {:d} = {:d}'.format(a, b, c))
