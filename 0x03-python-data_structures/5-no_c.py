3cbd2f2f8b3e63739b0e4fb298ea5f8d04545550
DevHeart1 <obasipeace412@yahoo.com>
2023-10-10 21:15:16 -0700

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    new_sty = ''
+    for q in range(len(my_string)):
+        if my_string[q] == 'c' or my_string[q] == 'C':
+            continue
+        new_str = new_str + my_string[q]
+    return new_ste
