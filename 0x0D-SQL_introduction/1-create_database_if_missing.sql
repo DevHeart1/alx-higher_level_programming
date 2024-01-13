d3e027ea43a6fd20542c57e891ccbffa6110d9bc
DevHeart <obasipeace412@yahoo.com>
2024-01-13 16:04:08 +0000

    create and delete database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- A script that creates the database hbtn_0c_0 in your MySQL server.
+-- The script doesn't fail if hbtn_0c_0 already exist.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- a script that deletes the database hbtn_0c_0 in your MySQL server.
+-- The script doesn't fail if hbtn_0c_0 already exist.
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
