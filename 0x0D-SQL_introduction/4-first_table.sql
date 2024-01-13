891da12bb2ba920cb839641c899f6fdc41f887d9
DevHeart <obasipeace412@yahoo.com>
2024-01-13 16:33:14 +0000

    creates a table.

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 -- Creates  table if it does not exist
-
-CREATE TABLE IF NOT EXISTS first_table(
-    id INT
-    name VARCHAR(256);
-)
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
