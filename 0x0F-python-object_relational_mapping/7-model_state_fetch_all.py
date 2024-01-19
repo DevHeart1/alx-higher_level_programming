5fc750e3cf957ec1418257c0696d87c65d5f8dd2
DevHeart1 <obasipeace412@yahoo.com>
2024-01-19 06:48:33 -0800

    Module that lists all states

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,14 +7,18 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    conn_str = "mysql://{}:{}@localhost:3306/{}".format(
+    conn_url = "mysql://{}:{}@localhost:3306/{}".format(
             argv[1], argv[2], argv[3])
-    engine = create_engine(conn_str, pool_pre_ping=True)
+    engine = create_engine(conn_url, pool_pre_ping=True)
+
+    Base.metadata.bind = engine
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
+
+    session.close()
