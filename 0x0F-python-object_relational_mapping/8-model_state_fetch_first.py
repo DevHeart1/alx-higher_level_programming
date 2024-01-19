db7e4fd154b891f3731d12360665e1bce265bda6
DevHeart1 <obasipeace412@yahoo.com>
2024-01-19 07:15:12 -0800

    prints the first state

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -7,16 +7,19 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    conn_str = "mysql://{}:{}@localhost:3306/{}".format(
+    conn_url = "mysql://{}:{}@localhost:3306/{}".format(
             argv[1], argv[2], argv[3])
-    engine = create_engine(conn_str, pool_pre_ping=True)
+    engine = create_engine(conn_url, pool_pre_ping=True)
+
+    Base.metadata.bind = engine
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).first()
+    first_state = session.query(State).order_by(State.id).first()
 
-    if (states):
-        print("{}: {}".format(states.id, states.name))
+    if (first_state):
+        print("{}: {}".format(first_state.id, first_state.name))
     else:
         print("Nothing")
+    session.close()
