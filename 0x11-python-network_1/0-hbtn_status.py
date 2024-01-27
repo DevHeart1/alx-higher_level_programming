cc7165dcb42fc24832fe40911328c4f0f22369ab
DevHeart1 <obasipeace412@yahoo.com>
2024-01-26 19:23:28 -0800

    network

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module with a python script that fetches url"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data.decode('utf-8')))
