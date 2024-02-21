634ca113c0e42262e66512c63b9a128cbe2a3d54
DevHeart1 <obasipeace412@yahoo.com>
2024-02-21 00:57:29 +0000

    a script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-if (process.argv.length !== 4) {
-  console.log('Pass in one file argument and its content');
-  process.exit(1);
-}
-
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (error) => {
   if (error) {
     console.error(error);
-  } else {
-    console.log('successfully written');
-  }
+  } 
 });
